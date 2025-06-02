fd26f169f865490bec88707e7fef529c1f2bd06b
Vijju <Vijju@mail.com>
2025-06-02 15:37:55 +0530

    Esko na raja chintakay pachadi

--- src/test/java/api/TestCases/userTest.java
+++ src/test/java/api/TestCases/userTest.java
@@ -68,7 +68,7 @@ public class userTest {
         Thread.sleep(3000);
         Response response = userEndpoints.deleteUser(this.usrPlds.getUsername());
         response.then().log().all();
-        Assert.assertEquals(response.getStatusCode(), 400);
+        Assert.assertEquals(response.getStatusCode(), 200);
         logger.info("User deleted successfully");
     }
 }
